b66798b3f3d2d6b43475486ed6e703cdab63897f
Unknown <nannerfox@me.com>
2018-04-14 09:15:26 -0500

    Switched Passport Strats
    removed facebook strat, installed local strat

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').Strategy;
 const bodyparser = require ('body-parser');
 const Sequelize = require("Sequelize");
 const express = require("express");
@@ -61,7 +60,6 @@ app.get('/beginning', (request, response) => {
     response.render('beginning');
 });
 app.use(express.static('views/images'));
-
 //PITA JOIN CODE (Part Two)
 app.get('/album', (req, res) => {
   Album.findAll({ include: [Artist]})
@@ -71,12 +69,6 @@ app.get('/album', (req, res) => {
       });
   });
 });
-
-app.use((request, response) => {
-    response.status(404);
-    response.render('404');
-});
-
 app.listen(app.get("port"), () => {
   console.log(
     "Express started on http://localhost:" +
@@ -85,32 +77,7 @@ app.listen(app.get("port"), () => {
   );
 });
 
-passport.use(new FacebookStrategy({
-    clientID: '428272014288040',
-    clientSecret: 'fe15c18d2ae3cf436c0f11e35b4913ca',
-    callbackURL: 'http://localhost:8080/auth/facebook/callback'
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // User.findOrCreate('...', function(err, user) {
-    //   if (err) { return done(err); }
-    //   done(null, user);
-    // });
-    console.log(profile)
-    done(profile)
-  }
-));
-
-// Redirect the user to Facebook for authentication.  When complete,
-// Facebook will redirect the user back to the application at
-//     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook'));
-
-// Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {
-    successRedirect: '/album',
-    failureRedirect: '/'
-    }));
+app.use((request, response) => {
+    response.status(404);
+    response.render('404');
+  });
